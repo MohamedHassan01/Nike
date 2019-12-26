70156662da4f568ad2ab2b12f35236aab9381f55
Mohamed <mohamed.pe20188@gmail.com>
2019-12-26 23:36:35 +0200

    Upload Project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav>
+    <v-toolbar flat>
+      <v-container>
+        <v-row>
+          <img
+            src="@/assets/nike-logo.png"
+            alt="logo"
+            width="64px"
+            height="64px"
+          />
+
+          <v-spacer></v-spacer>
+
+          <v-toolbar-items>
+            <v-btn text>New releases</v-btn>
+            <v-btn text>Men</v-btn>
+            <v-btn text>Kids</v-btn>
+            <v-btn text>variety</v-btn>
+          </v-toolbar-items>
+
+          <v-spacer></v-spacer>
+
+          <template class="icons" v-if="$vuetify.breakpoint.mdAndUp">
+            <v-btn icon class="icon">
+              <v-icon>mdi-shopping</v-icon>
+            </v-btn>
+            <v-btn icon class="icon">
+              <v-icon>mdi-search-web</v-icon>
+            </v-btn>
+          </template>
+        </v-row>
+      </v-container>
+    </v-toolbar>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'navbar'
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,135 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="2"
+        >
+          <div class="left-side">
+            <div class="slider-details">
+              <div class="number">
+                <span>{{ current }}</span> /
+                {{ totalProducts }}
+              </div>
+              <div class="progress-bar">
+                <span class="bar" ref="bar"></span>
+              </div>
+            </div>
+
+            <div class="social">
+              <h4>Follow</h4>
+              <div class="social-icons">
+                <v-btn fab text small>
+                  <v-icon >mdi-facebook</v-icon>
+                </v-btn>
+                <v-btn fab text small>
+                  <v-icon>mdi-linkedin</v-icon>
+                </v-btn>
+                <v-btn fab text small>
+                  <v-icon>mdi-twitter</v-icon>
+                </v-btn>
+              </div>
+            </div>
+          </div>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <div
+            v-for="slide in products"
+            :key="slide.id"
+          >
+            <div
+              class="home-content"
+              v-if="slide.isSelected"
+            >
+              <h2>{{ slide.title }}</h2>
+              <p>{{ slide.details }}</p>
+              <div class="buy">
+                <v-btn text depressed>Buy now</v-btn>
+                <span class="price">${{  slide.price }}</span>
+              </div>
+            </div>
+          </div>
+          <div class="see-more">
+            <p>See More</p>
+            <div class="btn">
+              <v-icon large>mdi-chevron-double-down</v-icon>
+            </div>
+          </div>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <div class="slider">
+            <div class="circle"></div>
+            <img class="selected" ref="imageSlider" src="../assets/home/01.png" alt="">
+            <div class="delimiter">
+              <v-btn class="prev" small fab text>
+                <v-icon>mdi-arrow-left</v-icon>
+              </v-btn>
+              <div
+                class="product"
+                v-for="product in products"
+                :key="product.id"
+              >
+                <div
+                  :class="[{'active': product.isSelected},'box']"
+                  @click="selectProduct(product)"
+                >
+                  <img :class="{'selected': product.isSelected}" :src="product.img" alt="">
+                </div>
+              </div>
+              <v-btn class="next" small fab text>
+                <v-icon>mdi-arrow-right</v-icon>
+              </v-btn>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "home",
   components: {
-    HelloWorld
+    
+  },
+
+  data() {
+    return {
+      current: '01',
+      totalProducts: '04',
+      products: [
+        {id: '1', img: require('@/assets/home/01.png'), isSelected: true, title: 'Nike running shoes', details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, repellendus voluptatibus assumenda cupiditate perferendis.', price: 199, value: '25%'},
+        {id: '2', img: require('@/assets/home/02.png'), isSelected: false, title: 'Nike women running shoes', details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, repellendus voluptatibus assumenda cupiditate perferendis.', price: 149, value: '50%'},
+        {id: '3', img: require('@/assets/home/03.png'), isSelected: false, title: 'Running Shoes vlue', details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, repellendus voluptatibus assumenda cupiditate perferendis.', price: 159, value: '75%'},
+        {id: '4', img: require('@/assets/home/04.png'), isSelected: false, title: 'Running Shoes black', details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, repellendus voluptatibus assumenda cupiditate perferendis.', price: 239, value: '100%'},
+      ]
+    }
+  },
+
+  methods: {
+    selectProduct(product) {
+      this.products.forEach(p => {
+        p.isSelected = false;
+      });
+      product.isSelected = true;
+
+      window.setTimeout(() => {
+        this.$refs.imageSlider.setAttribute('src', product.img);
+      }, 500);
+
+      this.$refs.bar.style.height = product.value
+      this.current = '0' + product.id;
+    }
   }
 };
 </script>
